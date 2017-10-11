6b88fac5244b5e0204ce67fcbc4910d83d1400aa
Liam Parker <parker.liam5@gmail.com>
2017-10-12 03:21:22 +1100

    JSON AJAX response test
    - tested sending a full json object back.

--- ajax_handler.php
+++ ajax_handler.php
@@ -14,21 +14,27 @@ if (isset($json_request) && $json_request !== "")
     // Sending a test response
     echo '{
         "names": [
-          "Tim",
-          "Bob",
-          "Alice",
-          "John",
-          "Samantha",
-          "Veronica"
-        ],
-        "values": [
-          12,
-          19,
-          3,
-          5,
-          2,
-          30
-        ]
+                "Tim",
+                "Bob",
+                "Alice",
+                "John",
+                "Samantha",
+                "Veronica"
+              ],
+              "datasets": [
+                {
+                  "label": "# of pets",
+                  "data": 
+                  [
+                      12,
+                      19,
+                      3,
+                      5,
+                      2,
+                      30
+                  ],
+                  "border-width": 1
+                }]
       }';
 }
 
--- javascript/dynamic.js
+++ javascript/dynamic.js
@@ -5,18 +5,14 @@ $(document).ready(function() {
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var json = JSON.parse(this.responseText);
-            console.log(json['names']);
+
             // Create chart
             var ctx = document.getElementById("chart").getContext("2d");
             var myChart = new Chart(ctx, {
                 type: 'bar',
                 data: {
                     labels: json['names'],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: json['values'],
-                        borderWidth: 1
-                    }]
+                    datasets: json['datasets']
                 },
                 options: {
                     scales: {
