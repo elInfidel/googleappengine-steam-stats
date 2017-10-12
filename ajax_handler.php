527621aebda74173efe981d8f1ea21a81231ef1d
Liam Parker <parker.liam5@gmail.com>
2017-10-12 22:35:46 +1100

    Ajax testing values

--- ajax_handler.php
+++ ajax_handler.php
@@ -2,36 +2,23 @@
 
 use Google\Cloud\Storage\StorageClient;
 
-
 $json_request = $_REQUEST["type"];
 
 if (isset($json_request) && $json_request !== "") 
 {   
     // Sending a test response
-    echo '{
-        "names": [
-                "Tim",
-                "Bob",
-                "Alice",
-                "John",
-                "Samantha",
-                "Veronica"
-              ],
-              "datasets": [
-                {
-                  "label": "# of pets",
-                  "data": 
-                  [
-                      12,
-                      19,
-                      3,
-                      5,
-                      2,
-                      30
-                  ],
-                  "border-width": 1
-                }]
-      }';
+    $obj = new stdClass;
+
+    $dataset = new stdClass;
+    $dataset->label = "# of pets";
+    $dataset->data = array(12, 19, 3, 5, 2, 30);
+
+    $labels = array("Tim","Bob","Alice","John","Samantha","Veronica");
+    $datasets = array($dataset);
+
+    $obj->data = array($labels, $dataset);
+
+    echo json_encode($obj);
 }
 
 ?>
